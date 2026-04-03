4e211d3f7a6698199880e2a07cda9214bb42ca49
Vinayak Rastogi <vinayakr841@gmail.com>
2026-04-03 23:44:33 +0530

    LL

--- Linked List/implementation.cpp
+++ Linked List/implementation.cpp
@@ -2,13 +2,26 @@
 using namespace std;
 
 class Node{
+    
+public:
     int data;
     Node* next;
 
-public:
+
     Node(int val){
         data = val;
         next = NULL;
+    }
+
+    ~Node(){
+        // cout<<"destructor of NODE "<<data<<endl;
+        if (next!=NULL)
+        {
+            
+            delete next;
+            next = NULL;
+        }
+        
     }
 
 };
@@ -23,11 +36,125 @@ public:
         tail = NULL;
 
     }
+
+   
+    ~List(){
+
+        //  cout<<"destructor list\n";
+        if (head!=NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        
+    }
+
+    void printList(){
+
+        Node* temp = head;
+
+        while(temp!=NULL){
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<"NULL"<<endl;
+
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val); //dynamic allocation 
+
+        if(head==NULL){
+            head = tail = newNode;
+
+        }else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val); //dynamic allocation 
+
+        if(head==NULL){
+            head = tail = newNode;
+
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void insert(int val, int pos){
+        Node* newNode = new Node(val);
+        Node* temp = head;
+        for (int i = 0; i < pos-1; i++)
+        {
+            temp = temp->next;
+        }
+
+        newNode->next = temp->next;
+        temp->next = newNode;
+        
+    }
+
+    void pop_front(){
+
+        if (head == NULL)
+        {
+            cout<<"LL is empty\n";
+            return;
+        }
+        
+
+        Node* temp = head;
+        head = head->next;
+
+        temp->next = NULL;
+        delete temp;
+        
+        
+    }
+    void pop_back(){
+        Node* temp = head;
+
+        if (tail == head)
+        {
+            cout<<"LL is empty\n";
+            return;
+        }
+        
+        
+
+        while(temp->next->next !=NULL){//or temp->next->next !=NULL
+            temp=temp->next;
+        }
+
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+        
+        
+        
+    }
 };
 
 
 int main(){
 
-    List ll();
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.printList();
+
+    ll.pop_back();
+
+    ll.printList();
+    ll.pop_back();
+    ll.printList();
+    ll.pop_back();
     return 0;
 }
