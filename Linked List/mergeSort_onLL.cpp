7477ff3c869cc818012ae460458cb9f07073e71a
Vinayak Rastogi <vinayakr841@gmail.com>
2026-05-22 18:09:17 +0530

    fix

--- Linked List/mergeSort_onLL.cpp
+++ Linked List/mergeSort_onLL.cpp
@@ -1,145 +1,162 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
+class Node
+{
 public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(int val){
+    Node(int val)
+    {
         data = val;
         next = NULL;
     }
-
 };
 
-class List{
+class List
+{
 public:
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
 
-    List(){
+    List()
+    {
         head = NULL;
         tail = NULL;
     }
 
-    void push_front(int val){
+    void push_front(int val)
+    {
         Node *newNode = new Node(val);
 
-        if(head == NULL){
+        if (head == NULL)
+        {
             head = tail = newNode;
-
-        }else{
+        }
+        else
+        {
             newNode->next = head;
             head = newNode;
         }
     }
 
-    void push_back(int val){
+    void push_back(int val)
+    {
         Node *newNode = new Node(val);
 
-        if(head == NULL){
+        if (head == NULL)
+        {
             head = tail = newNode;
-
-        }else{
+        }
+        else
+        {
             tail->next = newNode;
             tail = newNode;
         }
     }
 
-    void pop_front(){
-        if(head==NULL){
-            cout<<"LL is empty\n";
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            cout << "LL is empty\n";
             return;
-
         }
 
-        Node* temp = head;
+        Node *temp = head;
         head = head->next;
 
         temp->next = NULL;
         delete temp;
     }
-
-
-
 };
 
-Node* merge(Node* left, Node* right){
+Node *merge(Node *left, Node *right)
+{
     List ans;
-    Node* i = left;
-    Node* j= right;
+    Node *i = left;
+    Node *j = right;
 
-    while (i!=NULL && j!=NULL)
+    while (i != NULL && j != NULL)
     {
-        if(i->data <= j->data){
+        if (i->data <= j->data)
+        {
             ans.push_back(i->data);
             i = i->next;
-        }else{
+        }
+        else
+        {
             ans.push_back(j->data);
             j = j->next;
         }
     }
 
-    while(i!=NULL){
+    while (i != NULL)
+    {
         ans.push_back(i->data);
         i = i->next;
-
     }
 
-    while(j!=NULL){
+    while (j != NULL)
+    {
         ans.push_back(j->data);
         j = j->next;
     }
-    
+
     return ans.head;
 }
 
-Node* splitAtMid(Node* head){
-    Node* slow = head;
-    Node* fast = head;
-    Node* prev;
-    while(fast!=NULL && fast->next!=NULL){
+Node *splitAtMid(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    Node *prev;
+    while (fast != NULL && fast->next != NULL)
+    {
+
         prev = slow;
         slow = slow->next;
         fast = fast->next->next;
     }
 
-    if(prev != NULL){
+    if (prev != NULL)
+    {
         prev->next = NULL;
-    
     }
 
     return slow;
-    
 }
 
-Node* mergeSort(Node* head){
+Node *mergeSort(Node *head)
+{
 
-    if(head == NULL || head->next == NULL){
+    if (head == NULL || head->next == NULL)
+    {
         return head;
     }
 
-    Node* rightHead = splitAtMid(head);
-    Node* left = mergeSort(head);
-    Node* right = mergeSort(rightHead);
+    Node *rightHead = splitAtMid(head);
+    Node *left = mergeSort(head);
+    Node *right = mergeSort(rightHead);
     return merge(left, right);
 }
 
-void printList(Node* head)
+void printList(Node *head)
+{
+
+    Node *temp = head;
+
+    while (temp != NULL)
     {
-
-        Node *temp = head;
-
-        while (temp != NULL)
-        {
-            cout << temp->data << " -> ";
-            temp = temp->next;
-        }
-        cout << "NULL" << endl;
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
 }
 
-int main(){
+int main()
+{
     List ll;
 
     ll.push_front(2);
@@ -155,7 +172,6 @@ int main(){
     ll.head = mergeSort(ll.head);
 
     printList(ll.head);
-    
-    return 0;
 
+    return 0;
 }
